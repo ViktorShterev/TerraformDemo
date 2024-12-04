4ecb4242445917ff7de02d1b0b5bab96396eb519
Viktor Shterev <viktor.shterev55@gmail.com>
2024-12-04 09:00:20 +0200

    update format

--- main.tf
+++ main.tf
@@ -7,10 +7,10 @@ terraform {
   }
 
   backend "azurerm" {
-    resource_group_name = "StorageRG"
+    resource_group_name  = "StorageRG"
     storage_account_name = "taskboardstoragevik"
-    container_name = "taskboardcontainervik"
-    key = "terraform.tfstate"
+    container_name       = "taskboardcontainervik"
+    key                  = "terraform.tfstate"
   }
 }
 
